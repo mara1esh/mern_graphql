eddc6900dae03c9b499196bc57f928f30cab4402
valerii <valerasheligan@gmail.com>
2019-08-09 12:55:15 +0300

    add lodash. created authorType schema

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const schema = require('./schema/schema')
 const app = express()
 
 app.use('/graphql', graphqlHTTP({
-  schema
+  schema,
+  graphiql: true
 })) // middleware for listen graphql
 
 app.listen(4000, () => console.log('the server is running on port 4000'))
