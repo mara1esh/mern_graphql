fd29e8ef321ec2bbc35553161bc7f63a5fddc266
valerii <valerasheligan@gmail.com>
2019-08-09 11:18:07 +0300

    created book GraphQL schema

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+
+const schema = require('./schema/schema')
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+  schema
+})) // middleware for listen graphql
+
+app.listen(4000, () => console.log('the server is running on port 4000'))
